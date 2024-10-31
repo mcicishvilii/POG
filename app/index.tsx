4a7ff5c83066d75ab9c33bb5ca55574acfb16bfd
mcicishvilii <mcicishvilii@gmail.com>
2024-11-01 00:20:42 +0400

    linking to fb and x added

--- app/index.tsx
+++ app/index.tsx
@@ -80,21 +80,41 @@ const FirstScreen = () => {
       </View>
 
       <View style={styles.iconRow}>
-        <Image
-          style={styles.icon}
-          source={require("../assets/images/icons8-facebook-50.png")}
-          accessibilityLabel="X logo"
-        />
-        <Image
-          style={styles.icon}
-          source={require("../assets/images/icons8-x-50.png")}
-          accessibilityLabel="YouTube logo"
-        />
-        <Image
-          style={styles.icon}
-          source={require("../assets/images/icons8-youtube-50.png")}
-          accessibilityLabel="Facebook logo"
-        />
+        <TouchableOpacity
+          onPress={() =>
+            Linking.openURL("https://www.facebook.com/OfficialPOG/")
+          }
+        >
+          <Image
+            style={styles.icon}
+            source={require("../assets/images/icons8-facebook-50.png")}
+            accessibilityLabel="Facebook logo"
+          />
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => Linking.openURL("https://x.com/OfficialPOG?mx=2")}
+        >
+          <Image
+            style={styles.icon}
+            source={require("../assets/images/icons8-x-50.png")}
+            accessibilityLabel="X logo"
+          />
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() =>
+            Linking.openURL(
+              "https://www.youtube.com/channel/UC8rnaboNC0B1NgBNGWhRl1A?view_as=subscriber"
+            )
+          }
+        >
+          <Image
+            style={styles.icon}
+            source={require("../assets/images/icons8-youtube-50.png")}
+            accessibilityLabel="YouTube logo"
+          />
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
